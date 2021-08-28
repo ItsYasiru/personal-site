c9ba7de2c63ee9e0813dacccf10d68b8c84173e0
Yas_i_ru <mwmykd@gmail.com>
2021-08-28 23:38:59 +0530

    Internal refactoring

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,9 +1,12 @@
+const request = new XMLHttpRequest();
+
 const webhook = "https://discord.com/api/webhooks/881194999931371539/O2BOM8WIQjS_bzSky06QabKrSS60XyjpWXi9KtJZwpJ8eERtvrAK_mvoaikvOTnkgmmQ" // Leave it alone :'D
 
-function sendMessage(content) {
-    var request = new XMLHttpRequest();
-    request.open("POST", webhook);
+const form = document.getElementById('form');
+const formButton = document.getElementById('form-button');
 
+function sendMessage(content) {
+    request.open("POST", webhook);
     request.setRequestHeader('Content-type', 'application/json');
 
     var params = {
@@ -13,31 +16,30 @@ function sendMessage(content) {
     request.send(JSON.stringify(params));
 }
 
-function showForm() {
-    var form = document.getElementById('form');
-    var formButton1 = document.getElementById('form-button-1');
-    var formButton2 = document.getElementById('form-button-2');
-
+function openForm() {
     form.style.display = 'contents';
-    formButton1.style.display = 'none';
-    formButton2.style.display = 'block';
+    formButton.style.display = 'none';
+    window.location.href='#form'
 }
 
 function submitForm() {
     var name = document.getElementById('form-name').value;
     var email = document.getElementById('form-email').value;
 
-    if (name == '' || email == '') {
-        alert("A field cannot be empty!")
+    if (name == '') {
+        alert("Name cannot be empty!")
+        window.location.href='#form-name'
+    } else if (email == '') {
+        alert("Email cannot be empty!")
+        window.location.href='#form-email'
     } else {
-        sendMessage(`Client request Name: **'${name}'** Email: **'${email}'**`)
-
-        var form = document.getElementById('form');
-        var formButton1 = document.getElementById('form-button-1');
-        var formButton2 = document.getElementById('form-button-2');
-
         form.style.display = 'none';
-        formButton1.style.display = 'none';
-        formButton2.style.display = 'none';
+        sendMessage(`Client request Name: **'${name}'** Email: **'${email}'**`)
     }
+}
+
+function closeForm() {
+    window.location.href='#services'
+    form.style.display = 'none';
+    formButton.style.display = 'block';
 }
