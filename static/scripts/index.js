fbce55adae0364fba8b78a77a2d8ca6e582977d5
Yas_i_ru <mwmykd@gmail.com>
2021-09-08 12:06:42 +0530

    Added few lables to the header

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,6 +1,20 @@
-window.onscroll = function() {scrollFunction()};
-
+const lable = document.getElementById("lable");
 const toTopButton = document.getElementById("toTopButton");
+
+const lables = [
+    "Check out my <a href='#fiverr'>fiverr!</a>",
+    "My name is Yasiru and I like to code as a hobby."
+];
+
+var lableIndex = 0;
+
+window.onload = function() {
+    cycleLables();
+};
+
+window.onscroll = function() {
+    scrollFunction();
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -13,4 +27,13 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+function cycleLables() {
+    lableIndex++
+    if (lableIndex >= lables.length) {
+        lableIndex = 0
+    }
+    lable.innerHTML = lables[lableIndex]
+    setTimeout(cycleLables, 5 * 1000);
 }
