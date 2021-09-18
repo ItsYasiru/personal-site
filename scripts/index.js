f2b8d1ccbb40ff5a879e61016dab420a272e8aef
Yas_i_ru <mwmykd@gmail.com>
2021-09-18 20:57:04 +0530

    Mobile responsive changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const lable = document.getElementById("lable");
-const toTopButton = document.getElementById("toTopButton");
+const toTopButton = document.getElementById("to-top-btn");
 
 const lables = [
     "Check out my <a href='#fiverr'>fiverr!</a>",
@@ -36,4 +36,16 @@ function cycleLables() {
     }
     lable.innerHTML = lables[lableIndex]
     setTimeout(cycleLables, 5 * 1000);
+}
+
+function openMenu() {
+    var menu = document.getElementById("menu");
+    menu.className = "nav-menu";
+    menu.style.display = "inherit";
+    menu.style.width = "250px";
+    document.getElementById("menu-close-btn").style.display = "block";
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.width = "0";
 }
