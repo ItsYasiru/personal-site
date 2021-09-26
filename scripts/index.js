245a1124ab8726b79b2162f68d473cb895645f80
Yas_i_ru <mwmykd@gmail.com>
2021-09-26 20:35:58 +0530

    Nothing to mention

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-const lable = document.getElementById("lable");
+const lable = document.getElementById('lable');
 
 const lables = [
-    "Check out my <a href='#fiverr'>fiverr!</a>",
-    "My name is Yasiru and I like to code as a hobby."
+    'Check out my <a href="#fiverr">fiverr!</a>',
+    'My name is Yasiru and I like to code as a hobby.'
 ];
 
 var lableIndex = 0;
@@ -21,13 +21,13 @@ function cycleLables() {
 }
 
 function openMenu() {
-    var menu = document.getElementById("menu");
-    menu.className = "nav-menu";
-    menu.style.display = "inherit";
-    menu.style.width = "250px";
-    document.getElementById("menu-close-btn").style.display = "block";
+    var menu = document.getElementById('menu');
+    menu.className = 'nav-menu';
+    menu.style.display = 'inherit';
+    menu.style.width = '250px';
+    document.getElementById('menu-close-btn').style.display = 'block';
 }
 
 function closeMenu() {
-    document.getElementById("menu").style.width = "0";
+    document.getElementById('menu').style.width = '0';
 }
